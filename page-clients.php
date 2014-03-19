016cb0e6c744da62ca59e3d96ddca8c4656db86a
Charles Baker <assemblag3@gmail.com>
2014-03-18 22:03:31 -0400

    started table for logos on clients page

---
+++ page-clients.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+
+	<!-- content column -->
+	<div class="row page-main">
+
+		<div class="col-md-9 content-container">
+			
+			<div class="row">
+				<div class="col-md-12">
+					<div class="logo-title">
+						We assemble furniture from the following resellers:
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 logo-cell">
+					s
+				</div>
+				<div class="col-md-4 logo-cell">
+					s
+				</div>
+				<div class="col-md-4 logo-cell">
+					s
+				</div>
+			</div>
+
+		</div>
+
+		<!-- sidebar -->
+		<?php get_sidebar(); ?>
+
+	</div>
+
+<?php get_footer(); ?>
