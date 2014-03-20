1768d6028bc5dee197e12859f345a1e858cc8889
Charles Baker <assemblag3@gmail.com>
2014-03-20 00:43:16 -0400

    client page

--- page-clients.php
+++ page-clients.php
@@ -16,13 +16,49 @@
 
 			<div class="row">
 				<div class="col-md-4 logo-cell">
-					s
+					<img src="<?php bloginfo('template_directory'); ?>/logos/wal-mart.jpg" />
 				</div>
 				<div class="col-md-4 logo-cell">
-					s
+					<img src="<?php bloginfo('template_directory'); ?>/logos/kmart.jpg" />
 				</div>
 				<div class="col-md-4 logo-cell">
-					s
+					<img src="<?php bloginfo('template_directory'); ?>/logos/big-lots.jpg" />
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/sears.jpg" />
+				</div>
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/office-depot.jpg" />
+				</div>
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/target.jpg" />
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/staples.jpg" />
+				</div>
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/best-buy.jpg" />
+				</div>
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/office-max.jpg" />
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/lowes.jpg" />
+				</div>
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/big-sandy.jpg" />
+				</div>
+				<div class="col-md-4 logo-cell">
+					<img src="<?php bloginfo('template_directory'); ?>/logos/home-depot.jpg" />
 				</div>
 			</div>
 
